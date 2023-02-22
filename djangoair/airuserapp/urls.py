aa509bf689b00ff742bb3ff13b8eba17bb773f5c
Anastasia Mir <nastasia.ua@gmail.com>
2023-02-22 14:07:11 +0200

    ticket search

--- djangoair/airuserapp/urls.py
+++ djangoair/airuserapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
 
     path('tickets/', views.SearchTicketsView.as_view(), name='tickets'),
-    #path('tickets/details/', views.TicketDetailsView.as_view(), name='ticket details'),
+    path('tickets/details/', views.TicketDetailsView.as_view(), name='ticket details'),
     #path('tickets/booking/', views.TicketBookingView.as_view(), name='ticket booking'),
 
     path('ajax/load_dates/', views.load_dates, name='ajax load dates'),
--- djangoair/airuserapp/views.py
+++ djangoair/airuserapp/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import TemplateView, UpdateView, CreateView, FormView
 from django.views import View
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
 from airuserapp.forms import TicketForm
-from airstaffapp.models import Flight
+from airstaffapp.models import Flight, FlightDate
+from airuserapp.models import Ticket
 
 
 class HomeView(TemplateView):
@@ -16,7 +18,19 @@ class SearchTicketsView(View):
         return render(request, 'tickets.html', context)
 
     def post(self, request):
-        pass
+        destination = request.POST.get('destination')
+        date = FlightDate.objects.get(date=request.POST.get('date'))
+
+        passengers = request.POST.get('passengers')
+
+        flight = Flight.objects.get(destination=destination, date=date)
+        sold_tickets = len(Ticket.objects.filter(flight=flight).all())
+        if int(passengers) <= flight.passengers-sold_tickets:
+            Ticket.objects.create(flight=flight, tickets_quantity=passengers)
+            return redirect(reverse('passengers:ticket details'))
+        else:
+            messages.error(request, 'No tickets available. Please, choose another flight.')
+            return redirect('tickets')
 
 
 #ajax
@@ -26,6 +40,10 @@ def load_dates(request):
     dates = [flight.date for flight in flights]
     context = {'dates': dates}
     return render(request, 'dates_dropdown.html', context)
+
+
+class TicketDetailsView(UpdateView):
+    pass
 
 
 
