27648fb7e4e3ec0ec92022e49545065df4a6f343
Anastasia Mir <nastasia.ua@gmail.com>
2023-03-13 15:49:15 +0200

    messages dosplay fixed & airstaffapp temlates bootstrap added

--- djangoair/airstaffapp/forms.py
+++ djangoair/airstaffapp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Select, SelectMultiple, NumberInput, DateInput, TextInput
 
 from accounts.models import Staff
 from airstaffapp.models import Flight, FlightDate, LunchOptions, LuggageOptions
@@ -9,11 +9,20 @@ class StaffRoleEditForm(ModelForm):
         model = Staff
         fields = ['role']
 
+        widgets = {
+            'role': Select(attrs={'class': 'form-control'})
+        }
+
 
 class LunchOptionsForm(ModelForm):
     class Meta:
         model = LunchOptions
         fields = '__all__'
+
+        widgets = {
+            'description': TextInput(attrs={'class': 'form-control'}),
+            'price': NumberInput(attrs={'class': 'form-control'}),
+        }
 
 
 class LuggageOptionsForm(ModelForm):
@@ -21,14 +30,32 @@ class LuggageOptionsForm(ModelForm):
         model = LuggageOptions
         fields = '__all__'
 
+        widgets = {
+            'quantity': Select(attrs={'class': 'form-control'}),
+            'price': NumberInput(attrs={'class': 'form-control'}),
+        }
+
 
 class DateForm(ModelForm):
     class Meta:
         model = FlightDate
         fields = ['date']
 
+        widgets = {
+            'date': DateInput(attrs={'class': 'form-control'}),
+        }
+
 
 class FlightCreationForm(ModelForm):
     class Meta:
         model = Flight
         fields = ['destination', 'passengers', 'ticket_price', 'lunch', 'luggage', 'extra_luggage_price']
+
+        widgets = {
+            'destination': Select(attrs={'class': 'form-control'}),
+            'passengers': NumberInput(attrs={'class': 'form-control'}),
+            'ticket_price': NumberInput(attrs={'class': 'form-control'}),
+            'lunch': SelectMultiple(attrs={'class': 'form-control'}),
+            'luggage': SelectMultiple(attrs={'class': 'form-control'}),
+            'extra_luggage_price': NumberInput(attrs={'class': 'form-control'}),
+        }
