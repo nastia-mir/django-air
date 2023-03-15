a46981773002018315ec2f9be433bcab0ea8b5de
Anastasia Mir <nastasia.ua@gmail.com>
2023-03-15 16:02:21 +0200

    .env added

--- djangoair/djangoairproject/settings.py
+++ djangoair/djangoairproject/settings.py
@@ -127,10 +127,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = env('EMAIL_PORT')
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nastasia.ua@gmail.com'
-EMAIL_HOST_PASSWORD = 'zzwwokjdrkwmmknt'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 PASSWORD_RESET_TIMEOUT = 300
 
